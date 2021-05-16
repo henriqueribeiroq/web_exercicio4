a1badfb7dc990f334bdf3d101547384d1d919fcf
henriqueribeiroq <rickribeiro2007@gmail.com>
2021-05-15 23:12:17 -0300

    up

--- src/main/java/com/exercicio4/ex4/Ex4Application.java
+++ src/main/java/com/exercicio4/ex4/Ex4Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ex4Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(Ex4Application.class, args);
 	}
 
