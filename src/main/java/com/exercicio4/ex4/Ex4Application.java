7030bdbb7715c9b86d7c20eec475ff281136acb8
henriqueribeiroq <rickribeiro2007@gmail.com>
2021-05-15 23:42:19 -0300

    up

--- src/main/java/com/exercicio4/ex4/Ex4Application.java
+++ src/main/java/com/exercicio4/ex4/Ex4Application.java
@@ -8,6 +8,12 @@ public class Ex4Application {
 
 	public static void main(String[] args) { 
 		SpringApplication.run(Ex4Application.class, args);
+		System.out.println(
+			"Trabalho desenvolvido por José Henrique Ribeiro de Queiroz e Tiago Matias de Araújo."
+
+		);
+
+
 	}
 
 }
